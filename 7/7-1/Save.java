33d0ac5a3af3854a663f82fe251078fc32036245
sotakari <146611649+sotakari@users.noreply.github.com>
2024-05-13 14:42:48 +0000

    [Finish] 7-1

---
+++ 7/7-1/Save.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Save {
+  public void saveHeroToFile(Hero h) throws IOException {
+    Writer w = new BufferedWriter(new FileWriter("rpgsave.dat"));
+    w.write(h.name + "¥n");  // 名前の末尾に改行を付けて保存
+    w.write(h.hp + "¥n");    // HPの末尾に改行を付けて保存
+    w.write(h.mp + "¥n");    // MPの末尾に改行を付けて保存
+    w.flush();
+    w.close();
+  }
+}
