4b8af53ec5d221c6df1d3ca49a31176b4babeed9
sotakari <146611649+sotakari@users.noreply.github.com>
2024-06-24 15:41:42 +0000

    [Finish] quiz

---
+++ 10/quiz/2/target/site/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Bank","l":"Bank()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Bank","l":"equals(Object)","u":"equals(java.lang.Object)"}];updateSearchResults();
