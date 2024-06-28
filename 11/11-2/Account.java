b99910e5cc2f323ef60d99ba9f4a3a7ead1fcdb3
sotakari <146611649+sotakari@users.noreply.github.com>
2024-06-28 13:06:04 +0000

    [Fix] 11-2

--- 11/11-2/Account.java
+++ 11/11-2/Account.java
@@ -2,8 +2,8 @@ public class Account {
   String owner;     // 口座名義人
   int zandaka;      // 口座残高
   public Account(String owner, int zandaka) {
-    owner = owner;
-    zandaka = zandaka;
+    this.owner = owner;
+    this.zandaka = zandaka;
   }
   public void transfer(Account dest, int amount) {
     dest.zandaka += amount;
