38aea49e47261a5b20a71fc5dc349f51db97e070
sotakari <146611649+sotakari@users.noreply.github.com>
2024-07-09 22:22:04 +0000

    [Add]

---
+++ 10/quiz/1/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"jp.miyabilink.atm","c":"Bank","l":"addAccount(String, int)","u":"addAccount(java.lang.String,int)"},{"p":"jp.miyabilink.atm","c":"Bank","l":"Bank()","u":"%3Cinit%3E()"},{"p":"jp.miyabilink.atm","c":"Bank","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ 10/quiz/1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"jp.miyabilink.atm","l":"Bank"}];updateSearchResults();
