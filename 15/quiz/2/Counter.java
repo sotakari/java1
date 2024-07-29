7c351ee6d51925bad7dafc15d95b93fcf25cb4be
sotakari <146611649+sotakari@users.noreply.github.com>
2024-07-29 12:26:40 +0000

    [Finish] quiz 2

---
+++ 15/quiz/2/Counter.java
@@ -0,0 +1,15 @@
+vpublic class Counter {
+  private long count = 0;
+  public void add(long i) {
+    System.out.println("足し算します");
+    synchronized(this) {
+      this.count += i;
+    }
+  }
+  public void mul(long i){
+    System.out.println("掛け算します");
+    synchronized(this) {
+      this.count *= i;
+    }
+  }
+}
